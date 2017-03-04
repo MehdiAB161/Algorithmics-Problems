d991f0092382d3cae051511a7dc6413ccfd3bed5
MehdiAb161 <mehdi-b-@live.fr>
2017-03-04 22:14:48 +0100

    alternating-tasks

---
+++ Alternating-Tasks/main.py
@@ -0,0 +1,17 @@
+def timing(kate, yanna):
+	return sum(kate[::2]) + sum(yanna[1::2])
+
+
+def to_int(array):
+	return [int(i) for i in array] 
+
+T=int(input())
+N=int(input())
+
+i = 0
+while int(i)!= T:
+	yanna = to_int(input().split())
+	kate = to_int(input().split())
+	time = str(min(timing(kate, yanna), timing(yanna, kate)))
+	print(str(time))
+	i += 1
